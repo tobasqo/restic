a6b024282c0034f6c09a3570b71c363112e02b20
tobasqo <szymon.j.jaskowski@gmail.com>
2026-04-08 16:57:08 +0200

    simplify url validation

--- src/restic/utils/urls.py
+++ src/restic/utils/urls.py
@@ -1,17 +1,11 @@
-from pydantic import BaseModel, HttpUrl, ValidationError
+from pydantic import HttpUrl, ValidationError
 
 from restic.exceptions import ResticInvalidUrlError
 
 
-class UrlValidator(BaseModel):
-    url: HttpUrl
-
-
 def parse_url(url: str) -> str:
     try:
-        # noinspection PyTypeChecker
-        _ = UrlValidator(url=url)  # type: ignore[arg-type]
+        _ = HttpUrl(url)
     except ValidationError as exc:
         raise ResticInvalidUrlError(url) from exc
-
     return url.rstrip("/")
